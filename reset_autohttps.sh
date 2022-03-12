853100e650f1e55d4db163ba0c5b92c4c0b5d498
Matthew Brett <matthew.brett@gmail.com>
2022-03-12 12:42:21 +0000

    Backticks to quotes to prevent execution

--- reset_autohttps.sh
+++ reset_autohttps.sh
@@ -4,7 +4,7 @@
 # https://discourse.jupyter.org/t/trouble-getting-https-letsencrypt-working-with-0-9-0-beta-4/3583/5?u=matthew.brett
 
 echo "Please see https://github.com/jupyterhub/zero-to-jupyterhub-k8s/issues/2601#issuecomment-1050406952"
-echo "You might want to first run the `kubectl edit deploy autohttps` suggested there"
+echo "You might want to first run the 'kubectl edit deploy autohttps' suggested there"
 
 read -n1 -r -p "Press y to continue, any other key to cancel." key
 echo
