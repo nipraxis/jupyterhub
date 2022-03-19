c7d18facb6def83b47e0ced38f85603d997bdf7b
Matthew Brett <matthew.brett@gmail.com>
2022-03-19 18:39:45 +0000

    Minor refactor of vars.sh loading

--- vars.sh
+++ vars.sh
@@ -5,7 +5,3 @@ echo "Configuration for hub ${HUB_NAME}"
 source vars.sh.common
 # Specific config for this hub.
 source hubs/vars.sh.${HUB_NAME}
-# Allow default pool to further autoscale, if necessary.
-MAX_DEFAULT_NODES=6
-# Config; this one will often have secrets.
-CONFIG_YAML=jh-secrets/config.yaml.${HUB_NAME}
