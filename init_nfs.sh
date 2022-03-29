f87a2b72ee67a174fec63e0eb8202d1abfc08196
Matthew Brett <matthew.brett@gmail.com>
2022-03-29 01:12:47 +0100

    Try using IP address for NFS

--- init_nfs.sh
+++ init_nfs.sh
@@ -14,6 +14,19 @@ envsubst < nfs-configs/nfs_deployment_tpl.yaml | kubectl create -f -
 
 kubectl create -f nfs-configs/nfs_service.yaml
 
+# Wait for pod to start
+echo Control-C to stop this loop if it does not return in 30s or so.
+while :
+do
+    sleep 5
+    echo 'Checking if pod is running'
+    pod_running=$(kubectl get pods -o custom-columns=POD:metadata.name,STATUS:status.phase | grep nfs-server | grep Running)
+    if [ -n "$pod_running" ]; then break; fi
+done
+
+# Get cluster IP
+NFS_IP=$(kubectl get service --namespace $NAMESPACE nfs-server -o yaml | grep "clusterIP:" | awk '//{print $2}')
+
 # Set up PV, PVC for home dirs and data directory.
 export NAMESPACE
 export NFS_PV_NAME="nfs"
