a94aa7e0dd98468ba426e235aaf33a4e123346af
Matthew Brett <matthew.brett@gmail.com>
2022-04-02 15:24:45 +0100

    Correctly tear down nfs

--- teardown_jhub.sh
+++ teardown_jhub.sh
@@ -2,7 +2,7 @@
 . set_config.sh
 echo "Deleting JHub $RELEASE"
 helm delete $RELEASE
-sleep 20
+sleep 10
 ./teardown_nfs.sh
-sleep 20
+sleep 10
 kubectl delete namespace $NAMESPACE
--- teardown_nfs.sh
+++ teardown_nfs.sh
@@ -2,5 +2,8 @@
 . set_config.sh
 echo "Deleting persistent volumes"
 
-kubectl delete persistentvolumes nfs
-kubectl delete persistentvolumes nfs-data
+kubectl delete service nfs-server
+kubectl delete deployments nfs-server
+# https://stackoverflow.com/questions/57401526/how-to-delete-persistent-volumes-in-kubernetes
+kubectl delete pvc nfs
+kubectl delete pvc nfs-data
